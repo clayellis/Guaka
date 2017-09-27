41b3a2066090001e2bf98e4db21ba898a3de6f45
Clay Ellis <clayellis@me.com>
2017-09-27 12:13:17 -0600

    Update package

--- Package.swift
+++ Package.swift
@@ -1,12 +1,17 @@
 // swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
   name: "Guaka",
+  products: [
+    .library(name: "Guaka", targets: ["Guaka"])
+    ],
   dependencies: [
-    .package(url: "https://github.com/clayellis/StringScanner.git", from: "0.3.1")
+    .package(url: "https://github.com/clayellis/StringScanner.git", from: "0.4.0")
     ],
   targets: [
-    .target(name: "Guaka")
-  ]
+    .target(name: "Guaka", dependencies: ["StringScanner"])
+  ],
+  swiftLanguageVersions: [4]
 )
